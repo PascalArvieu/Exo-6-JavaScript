1675007c247d95463539c543cb7ffcef9289da4a
PascalArvieu <pascalarvieu@gmail.com>
2020-11-03 14:45:01 +0100

    exo 6 JS

---
+++ Assets/JS/script.js
@@ -0,0 +1,39 @@
+// // Exercice 1
+// var coincoin = "Bien penser à mettre la ligne \"script type\" en bas, juste avant la fermeture de la balise \"body\"";
+// alert(coincoin)
+
+// // Exercice 2
+// var lastName = "John";
+// var firstName = "Doe";
+// var city = "Chicago";
+//     alert("Nom : " + lastName + "\n" + "Prénom : " + firstName + "\n" + "Ville : " + city);
+
+
+// Exercice 3
+// var firstName = prompt("C'est quoi ton blaze ?", "<Entrez ici votre prénom>");
+//     alert("Bonjour " + firstName + ", ça boume ?");
+
+// Exercice 4
+// function OKgood() {
+//     var firstName = document.getElementById("firstName").value;
+//     var lastName = document.getElementById("lastName").value;
+//     var city = document.getElementById("city").value;
+
+//     alert("Prénom : " + firstName + "\n" + "Nom : " + lastName + "\n" + "Ville : " + city);
+
+// // Exercice 5 
+// function keskiReste() {
+//     var dividende = document.getElementById("valeurX").value;
+//     var diviseur = document.getElementById("valeurY").value;
+//     var resultat = dividende % diviseur;
+
+//     alert("le reste de " + dividende + " divisé par " + diviseur + " est : " + resultat) 
+// }
+
+// Exercice 6 
+function footfoot() {
+    var num1 = document.getElementById("pointure").value;
+    var num2 = document.getElementById("age").value;
+    var resultat = (num1 * 2 + 5) * 50 - num2 + 1769;
+    alert("resultat" + resultat);
+}
